5799f2c9b0fb36fc256ee2cbfaf28345b37dbefe
dyhun <dyhun@users.noreply.github.com>
2016-12-28 14:07:02 +0200

    adding firebase instance

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -27,6 +27,8 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,13 +46,19 @@ public class MainActivity extends AppCompatActivity {
 
   private String mUsername;
 
+  private FirebaseDatabase mFirebaseDatabase;
+  private DatabaseReference mMessageDatabaseReference;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     mUsername = ANONYMOUS;
-
+    // Firebase
+    mFirebaseDatabase = FirebaseDatabase.getInstance();
+    mMessageDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+    
     // Initialize references to views
     mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
     mMessageListView = (ListView) findViewById(R.id.messageListView);
